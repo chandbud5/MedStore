bd96ac39cd2da31e234b58f3f75fb8918e9db68b
Chand Bud <chandbud5@gmail.com>
2020-09-21 13:03:35 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -7,21 +7,27 @@ public class Login{
     }
 }
 
-class Form extends JFrame {
+class Form{
     public Form(){
 
-        JLabel l1 = new JLabel("Login to your MedStore Account");
-        JLabel l2 = new JLabel("UserName");
-        JLabel l3 = new JLabel("Password");
-        JTextField t1 = new JTextField(18);
-        JTextField t2 = new JTextField(18);
+        JFrame frame = new JFrame();
+        JLabel l1 = new JLabel("Login to your MedStore Account", 0);
+        JLabel l2 = new JLabel("UserName", 0);
+        JLabel l3 = new JLabel("Password", 0);
+        JTextField t1 = new JTextField(40);
+        JTextField t2 = new JTextField(40);
         JButton submit = new JButton("Submit");
         JLabel m = new JLabel("Don't have account? Create One");
         JButton reg = new JButton("Register");
 
         JPanel jp1 = new JPanel(new BorderLayout(5, 20));
-        JPanel jp2 = new JPanel(new BorderLayout(5, 20));
+        JPanel jp2 = new JPanel(new BorderLayout(10, 20));
         JPanel jp3 = new JPanel(new BorderLayout(5, 20));
+
+        l1.setFont(new Font(l1.getName(), Font.PLAIN,20));
+        l2.setFont(new Font(l2.getName(), Font.PLAIN,16));
+        l3.setFont(new Font(l3.getName(), Font.PLAIN,16));
+        m.setFont(new Font(m.getName(), Font.PLAIN,18));
 
         jp1.add(l1, BorderLayout.NORTH);
         jp1.add(l2, BorderLayout.LINE_START);
@@ -30,18 +36,18 @@ class Form extends JFrame {
         jp2.add(l3, BorderLayout.LINE_START);
         jp2.add(t2, BorderLayout.LINE_END);
         jp2.add(submit, BorderLayout.SOUTH);
-
+        
         jp3.add(m, BorderLayout.LINE_START);
         jp3.add(reg, BorderLayout.LINE_END);
 
-        setLayout(new FlowLayout(FlowLayout.CENTER, 30, 10));
-        add(jp1);
-        add(jp2);
-        add(jp3);
-        setTitle("Login");
-        setResizable(false);
-        setVisible(true);
-        setSize(600, 480);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 10));
+        frame.add(jp1);
+        frame.add(jp2);
+        frame.add(jp3);
+        frame.setTitle("Login Form");
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setBounds(100, 25, 960, 700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
